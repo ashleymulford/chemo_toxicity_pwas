f44d986c892403b9a274ae4ba13b3fb382306861
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-10-16 14:36:54 -0400

    Update predict_proteome.sh

--- scripts/predict_proteome.sh
+++ scripts/predict_proteome.sh
@@ -1,5 +1,7 @@
 #do for all pops and models:
 
+#to use prot models data must be in build 38, ids must be chr#:pos
+#see liftover.sh for liftover info
 
 python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
 --model_db_path /home/ryan/topmed/proteome/dapg_net_CORRECT_WINDOWS/current_dbs/PAV_filteredALL_WG_baseline_rho0.1_zpval0.05.db \
