7e45a44a0df58ee6c2aab7518692d4c684bd2f07
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-11-10 14:17:38 -0500

    Create pred_exp_to_gemma_input.R

---
+++ scripts/pred_exp_to_gemma_input.R
@@ -0,0 +1,28 @@
+
+#Modified version of angela's script
+
+#converting predicted expression to GEMMA input
+#this is very unnecessarily clunky and can probably done much faster with half the code
+
+library(data.table)
+"%&%" = function(a,b) paste(a,b,sep="")
+model_list <- c("AFA", "CAU", "HIS", "CHN", "ALL")
+
+for(model in model_list){
+  pred_exp <- fread("/home/ashley/LCL_chemotherapy/YRI/YRI_pwas_results/predicted_levels/YRI_PAV_filtered" %&% model %&% "_WG_baseline_rho0.1_zpval0.05.txt", header = F) #path to PrediXcan-produced _predicted_expression.txt
+  pred_exp$V1 <- NULL
+  pred_exp$V2 <- NULL
+  pred_exp <- transpose(pred_exp)
+  gene_list <- as.data.frame(pred_exp$V1)
+  colnames(gene_list) <- "V0"
+  pred_exp <- cbind(gene_list, pred_exp)
+  pred_exp$V0 <- NA
+  pred_exp$V1 <- NA
+  colnames(gene_list) <- "V"
+  pred_exp <- cbind(gene_list, pred_exp)
+  fwrite(pred_exp, "/home/ashley/LCL_chemotherapy/YRI/YRI_pwas_results/association_input/YRI_PAV_filtered" %&% model %&% "_WG_baseline_rho0.1_zpval0.05_gemma.txt", na = "NA", quote = F, sep = "\t", col.names = F) #path to GEMMA input/pseudo-genotype
+}
+
+
+
+
