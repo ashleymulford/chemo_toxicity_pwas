0f80c74530d0860c085c5c76c761208f20cd9168
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-11-11 16:35:14 -0500

    Update significant_hits.R

--- scripts/significant_hits.R
+++ scripts/significant_hits.R
@@ -7,28 +7,37 @@ library(tibble)
 
 #Create list of tissues for file input
 model_list <- c("ALL", "AFA", "CAU", "CHN", "HIS")
-drug_list <- c()
-
-
-
-
-
+drug_list <- c("arac", "cape", "carbo", "cis", "dauno", "etop", "pacl", "peme")
 
 
 #Make a data frame with all results from each tissue per drug per pop
   #Read in file
   #Add column containing tissue name
   #Compile significant subsets into single data frame
-for(model in model_list){
-  arac_output <- fread("/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output/YRI_assoc_arac_" %&% tiss %&% ".assoc.txt")
-  arac_output<-add_column(arac_output, tissue = tiss, .before = "chr")
-  if(exists("YRI_assoc_arac")){
-    YRI_assoc_arac<-merge(x = YRI_assoc_arac, y = arac_output, all = TRUE)
-  }
-  else{
-    YRI_assoc_arac<-arac_output
+for(drug in drug_list){
+  for(model in model_list){
+    output <- fread("/home/ashley/LCL_chemotherapy/YRI/YRI_pwas_results/adj_assoc_output/YRI_" %&% drug %&% "_PAV_filtered" %&% model %&% "_WG_baseline_rho0.1_zpval0.05.adj.txt")
+    output <- add_column(output, model = model, .before = "chr")
+    output <- add_column(output, drug = drug, .before = "chr")
+    if(exists("all_assoc")){
+      all_assoc<-merge(x = all_assoc, y = output, all = TRUE)
+    }
+    else{
+      all_assoc<-output
+    }
   }
 }
 
+all_sig<-subset(all_assoc, pvalues_adjusted_BH < .75)
+
 #Output data frames into directory
-fwrite(YRI_assoc_arac, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_predixcan_arac", na = "NA", quote = F, sep = "\t", col.names = T) 
+fwrite(all_assoc, "/home/ashley/LCL_chemotherapy/YRI/YRI_pwas_results/adj_assoc_output/YRI_allassoc_PAV_filtered_WG_baseline_rho0.1_zpval0.05.adj.txt", na = "NA", quote = F, sep = "\t", col.names = T) 
+fwrite(all_sig, "/home/ashley/LCL_chemotherapy/YRI/YRI_pwas_results/adj_assoc_output/YRI_sig_PAV_filtered_WG_baseline_rho0.1_zpval0.05.adj.txt", na = "NA", quote = F, sep = "\t", col.names = T) 
+
+
+
+
+
+
+
+
