6b7a08081dfd6c27ac6e638996d049d75872b011
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-11-19 15:30:15 -0500

    Update predict_proteome.sh

--- scripts/predict_proteome.sh
+++ scripts/predict_proteome.sh
@@ -4,7 +4,7 @@
 #see liftover.sh for liftover info
 
 python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
---model_db_path /home/ryan/topmed/proteome/dapg_net_CORRECT_WINDOWS/current_dbs/PAV_filteredALL_WG_baseline_rho0.1_zpval0.05.db \
+--model_db_path /home/ryan/TOPMed_Proteome/dbs_out/AFA_PCAIR_baseline_models_PAV_filtered_rho0.1_zpval0.05.db \
 --text_genotypes /home/ashley/LCL_chemotherapy/ALL/ALL_predixcan_dosages/build_38/chr*.txt.gz \
 --text_sample_ids /home/ashley/LCL_chemotherapy/ALL/samples.txt \
---prediction_output /home/ashley/LCL_chemotherapy/ALL/ALL_pwas_results/predicted_levels/ALL_PAV_filteredALL_WG_baseline_rho0.1_zpval0.05.txt
+--prediction_output /home/ashley/LCL_chemotherapy/ALL/ALL_pwas_results/predicted_levels/ALL_PCAIR_PAV_filtered_AFA_baseline_rho0.1_zpval0.05.txt
