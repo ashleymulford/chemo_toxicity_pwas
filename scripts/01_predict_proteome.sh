1cf69ef61395390fcee16d8295c49432588b9089
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-11-19 15:54:21 -0500

    Update 01_predict_proteome.sh

--- scripts/01_predict_proteome.sh
+++ scripts/01_predict_proteome.sh
@@ -8,3 +8,21 @@ python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
 --text_genotypes /home/ashley/LCL_chemotherapy/ALL/ALL_predixcan_dosages/build_38/chr*.txt.gz \
 --text_sample_ids /home/ashley/LCL_chemotherapy/ALL/samples.txt \
 --prediction_output /home/ashley/LCL_chemotherapy/ALL/ALL_pwas_results/predicted_levels/ALL_PCAIR_PAV_filtered_AFA_baseline_rho0.1_zpval0.05.txt
+
+python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
+--model_db_path /home/ryan/TOPMed_Proteome/dbs_out/AFA_PCAIR_baseline_models_PAV_filtered_rho0.1_zpval0.05.db \
+--text_genotypes /home/ashley/LCL_chemotherapy/YRI/YRI_predixcan_dosages/build_38/chr*.txt.gz \
+--text_sample_ids /home/ashley/LCL_chemotherapy/YRI/samples.txt \
+--prediction_output /home/ashley/LCL_chemotherapy/YRI/YRI_pwas_results/predicted_levels/YRI_PCAIR_PAV_filtered_AFA_baseline_rho0.1_zpval0.05.txt
+
+python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
+--model_db_path /home/ryan/TOPMed_Proteome/dbs_out/AFA_PCAIR_baseline_models_PAV_filtered_rho0.1_zpval0.05.db \
+--text_genotypes /home/ashley/LCL_chemotherapy/CEU/CEU_predixcan_dosages/build_38/chr*.txt.gz \
+--text_sample_ids /home/ashley/LCL_chemotherapy/CEU/samples.txt \
+--prediction_output /home/ashley/LCL_chemotherapy/CEU/CEU_pwas_results/predicted_levels/CEU_PCAIR_PAV_filtered_AFA_baseline_rho0.1_zpval0.05.txt
+
+python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
+--model_db_path /home/ryan/TOPMed_Proteome/dbs_out/AFA_PCAIR_baseline_models_PAV_filtered_rho0.1_zpval0.05.db \
+--text_genotypes /home/ashley/LCL_chemotherapy/ASN/ASN_predixcan_dosages/build_38/chr*.txt.gz \
+--text_sample_ids /home/ashley/LCL_chemotherapy/ASN/samples.txt \
+--prediction_output /home/ashley/LCL_chemotherapy/ASN/ASN_pwas_results/predicted_levels/ASN_PCAIR_PAV_filtered_AFA_baseline_rho0.1_zpval0.05.txt
